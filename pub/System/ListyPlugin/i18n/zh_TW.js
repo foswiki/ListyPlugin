96b6819726c23c2d1c401a4135ed2ca2279d1d43
CH yang <dr3939@gmail.com>
2017-01-31 08:45:28 +0100

    Item13990: Translated using Weblate (Chinese (Taiwan))
    Currently translated at 100% (0 of 0 strings)
    
    Created new translation.

---
+++ pub/System/ListyPlugin/i18n/zh_TW.js
@@ -0,0 +1,7 @@
+{
+    "Cancel": "Cancel",
+    "Confirmation required": "Confirmation required",
+    "Delete Listy Item": "Delete Listy Item",
+    "OK": "OK",
+    "sorry, can't drop elements here": "sorry, can't drop elements here"
+}
