3edadbce4da22acc98a158b99a806f0a7b7bb5a7
CH yang <dr3939@gmail.com>
2017-01-31 08:47:05 +0100

    Item13990: Translated using Weblate (Chinese (Taiwan))
    Currently translated at 100.0% (5 of 5 strings)

--- pub/System/ListyPlugin/i18n/zh_TW.js
+++ pub/System/ListyPlugin/i18n/zh_TW.js
@@ -1,7 +1,7 @@
 {
-    "Cancel": "Cancel",
-    "Confirmation required": "Confirmation required",
-    "Delete Listy Item": "Delete Listy Item",
-    "OK": "OK",
-    "sorry, can't drop elements here": "sorry, can't drop elements here"
+    "Cancel": "取消", 
+    "Confirmation required": "需要確認", 
+    "Delete Listy Item": "刪除 Listy 項目", 
+    "OK": "OK", 
+    "sorry, can't drop elements here": "對不起，不能在這放置元件"
 }
